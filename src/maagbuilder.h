910b63a18df4c244936e7229447b8385ce588273
Vadim N <vd.naz@yandex.ru>
2015-03-15 20:41:59 +0300

    add draft file for MAAG Builder

---
+++ src/maagbuilder.h
@@ -0,0 +1,56 @@
+/*
+ * Ymir <imminfo.github.io/ymir>
+ *
+ * This file is part of Ymir, a fast C++ tool for computation of assembling
+ * probabilities, statistical inference of assembling statistical model
+ * and generation of artificial sequences of T-cell receptors data.
+ *
+ *
+ * Copyright 2015 Vadim Nazarov <vadim dot nazarov at mailbox dot com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _YMIR_MAAGBUILDER_H_
+#define _YMIR_MAAGBUILDER_H_
+
+
+#include "maag.h"
+
+
+namespace ymir {
+
+    class MAAGBuilder;
+
+
+    class MAAGBuilder : protected MAAG {
+    public:
+
+
+        MAAGBuilder() : MAAG() {
+
+        }
+
+
+        virtual ~MAAGBuilder() {
+        }
+
+
+
+
+    protected:
+
+    };
+}
+
+#endif //_YMIR_MAAGBUILDER_H_

558bfcbf82fde025ae167a60061830279dd865ec
Vadim Nazarov <vdn@mailbox.org>
2015-04-10 13:26:29 +0300

    add reading Matrix and VectorList file types

--- src/textdata.h
+++ src/textdata.h
@@ -5,6 +5,8 @@
 #ifndef YMIR_TEXTDATA_H
 #define YMIR_TEXTDATA_H
 
+#include <vector>
+
 #include "types.h"
 
 
@@ -15,39 +17,47 @@ namespace ymir {
     public:
 
         AbstractTDContainer(bool skip_first_column, prob_t laplace) {
-            _rownames.reserve(40);
             _colnames.reserve(40);
+            _rownames.reserve(40);
+            _data.reserve(40);
             _laplace = laplace;
             _skip_first_column = skip_first_column;
         }
 
+
         virtual ~AbstractTDContainer() { }
 
-        void set_laplace(prob_t laplace) { _laplace = laplace; }
 
-        void add_row_name(const string& name) { _rownames.push_back(name); }
+        void addRowName(const string& name) { _rownames.push_back(name); }
 
-        void add_column_name(const string& name) { _colnames.push_back(name); }
 
-        const vector<string>& rownames() const { return _rownames; }
+        void addColumnName(const string& name) { _colnames.push_back(name); }
 
-        const vector<string>& colnames() const { return _colnames; }
 
-        virtual void addRow(const vector<prob_t> vec) = 0;
+        const vector<string>& row_names() const { return _rownames; }
 
-        virtual bool read(const string& filepath) = 0;
+
+        const vector<string>& column_names() const { return _colnames; }
+
+
+        virtual bool read(const string& filepath, string &err_message) = 0;
+
+
+        const vector<prob_t>& data(size_t i) const { return _data[i]; }
 
     protected:
 
-        vector<string> _rownames;
+        vector< vector<prob_t> > _data;
         vector<string> _colnames;
+        vector<string> _rownames;
         prob_t _laplace;
         bool _skip_first_column;
 
 
         AbstractTDContainer() {
-            _rownames.reserve(40);
             _colnames.reserve(40);
+            _rownames.reserve(40);
+            _data.reserve(40);
             _laplace = 0;
             _skip_first_column = true;
         }
@@ -55,59 +65,94 @@ namespace ymir {
     };
 
 
+    /**
+     * \brief List of vectors for deletions, insertions and gene segments.
+     */
     struct TDVectorList : public AbstractTDContainer {
     public:
 
         TDVectorList(bool skip_first_column = true, prob_t laplace = 0) : AbstractTDContainer(skip_first_column, laplace) { }
 
+
         virtual ~TDVectorList() { }
 
-        void addRow(const vector<prob_t> vec) {
 
-        }
-
-        bool read(const string& filepath) {
+        bool read(const string& filepath, string &err_message) {
             ifstream ifs;
+
             ifs.open(filepath);
 
             if (ifs.is_open()) {
                 stringstream line_stream;
                 string line, word;
-                bool read_header = true;
+                vector<prob_t> word_vec;
+                int line_num = 1;
+                bool skip_col_num_check = true;
                 while (!ifs.eof()) {
                     getline(ifs, line);
                     if (line[0] != '\n') {
+                        word_vec.clear();
                         line_stream.str(line);
-                        if (read_header) {
+                        int i = !_skip_first_column;
+
+                        if (skip_col_num_check) {
                             while (!line_stream.eof()) {
                                 getline(line_stream, word, '\t');
-                                cout << word << endl;
-                                // add row
+                                if (i) {
+                                    this->addColumnName(word);
+                                    _data.push_back(vector<prob_t>());
+                                    _data[_data.size() - 1].reserve(40);
+                                }
+                                ++i;
                             }
-                            read_header = false;
                         } else {
-                            getline(line_stream, word, '\t'); // skip row's name
-                            int i = 0;
                             while (!line_stream.eof()) {
                                 getline(line_stream, word, '\t');
                                 // MPFR?!?!?! I don't know
                                 // add row
-                                cout << word << endl;
+                                if (i) { word_vec.push_back(stod(word)); }
+                                else { this->addRowName(word); }
                                 ++i;
                             }
+
                         }
                         line_stream.clear();
                     }
+
+                    if (!skip_col_num_check) {
+                        if (word_vec.size() == _colnames.size()) {
+                            for (size_t i = 0; i < word_vec.size(); ++i) {
+                                _data[i].push_back(word_vec[i]);
+                            }
+                        } else {
+                            stringstream ss;
+                            ss << "ERROR: number of elements doesn't match the number of columns in the line " <<
+                            (int) line_num <<
+                            " (expected: " <<
+                            (int) _colnames.size() <<
+                            ", got: " <<
+                            (int) word_vec.size() << ")";
+                            err_message = ss.str();
+                            return false;
+                        }
+                    } else {
+                        skip_col_num_check = false;
+                    }
+
+                    ++line_num;
                 }
+                ifs.close();
                 return true;
             }
 
+            err_message = "ERROR: can't open file [" + filepath + "]";
             return false;
         }
 
     protected:
 
         TDVectorList() : AbstractTDContainer() {}
+
     };
 
 
@@ -116,13 +161,74 @@ namespace ymir {
 
         TDMatrix(bool skip_first_column = true, prob_t laplace = 0) : AbstractTDContainer(skip_first_column, laplace)  { }
 
+
         virtual ~TDMatrix() { }
 
-        void addRow(const vector<prob_t> vec) {
 
-        }
+        bool read(const string& filepath, string &err_message) {
+            ifstream ifs;
 
-        bool read(const string& filepath) {
+            ifs.open(filepath);
+
+            if (ifs.is_open()) {
+                _data.push_back(vector<prob_t>());
+                stringstream line_stream;
+                string line, word;
+                vector<prob_t> word_vec;
+                int line_num = 1;
+                bool skip_col_num_check = true;
+                while (!ifs.eof()) {
+                    getline(ifs, line);
+                    if (line[0] != '\n') {
+                        word_vec.clear();
+                        line_stream.str(line);
+                        int i = !_skip_first_column;
+
+                        if (skip_col_num_check) {
+                            while (!line_stream.eof()) {
+                                getline(line_stream, word, '\t');
+                                if (i) { this->addColumnName(word); }
+                                ++i;
+                            }
+                        } else {
+                            while (!line_stream.eof()) {
+                                getline(line_stream, word, '\t');
+                                // MPFR?!?!?! I don't know
+                                // add row
+                                if (i) { word_vec.push_back(stod(word)); }
+                                else { this->addRowName(word); }
+                                ++i;
+                            }
+
+                        }
+                        line_stream.clear();
+                    }
+
+                    if (!skip_col_num_check) {
+                        if (word_vec.size() == _colnames.size()) {
+                            _data[0].insert(_data[0].end(), word_vec.begin(), word_vec.end());
+                        } else {
+                            stringstream ss;
+                            ss << "ERROR: number of elements doesn't match the number of columns in the line " <<
+                            (int) line_num <<
+                            " (expected: " <<
+                            (int) _colnames.size() <<
+                            ", got: " <<
+                            (int) word_vec.size() << ")";
+                            err_message = ss.str();
+                            return false;
+                        }
+                    } else {
+                        skip_col_num_check = false;
+                    }
+
+                    ++line_num;
+                }
+                ifs.close();
+                return true;
+            }
+
+            err_message = "ERROR: can't open file [" + filepath + "]";
             return false;
         }
 
@@ -139,13 +245,11 @@ namespace ymir {
 
         TDMatrixList(bool skip_first_column = true, prob_t laplace = 0) : AbstractTDContainer(skip_first_column, laplace)  { }
 
+
         virtual ~TDMatrixList() { }
 
-        void addRow(const vector<prob_t> vec) {
 
-        }
-
-        bool read(const string& filepath) {
+        bool read(const string& filepath, string &err_message) {
             return false;
         }
 
@@ -167,7 +271,6 @@ namespace ymir {
                                        prob_t laplace,
                                        string& err_message) {
         AbstractTDContainer* container;
-        bool ok;
         err_message = "OK";
         if (filetype == "matrix") {
             container = new TDMatrix(skip_first_column, laplace);
@@ -183,72 +286,10 @@ namespace ymir {
             }
             return nullptr;
         }
-        ok = container->read(filepath);
-        if (!ok) {
-            err_message  = "ERROR: problems in reading file [" + filepath + "]";
-        }
+
+        container->read(filepath, err_message);
         return container;
     }
-
-
-    /**
-    * \function read_matrix
-    *
-    * \brief
-    *
-    * \param filepath Path to the file with matrix, separated by spaces / tabs.
-    *
-    * \return New struct with matrix.
-    */
-//    NamedVectorArray read_vector_list(const string& filepath) {
-//
-//        ifstream ifs;
-//        ifs.open(filepath);
-//
-//        if (ifs.is_open()) {
-//            NamedVectorArray narr;
-//            stringstream line_stream;
-//            string line, word;
-//            bool read_header = true;
-//            while (!ifs.eof()) {
-//                getline(ifs, line);
-//                if (line[0] != '\n') {
-//                    line_stream.str(line);
-//                    if (read_header) {
-//                        while (!line_stream.eof()) {
-//                            getline(line_stream, word, '\t');
-//                            narr.addColumn(word);
-//                        }
-//                        read_header = false;
-//                    } else {
-//                        getline(line_stream, word, '\t'); // skip row's name
-//                        int i = 0;
-//                        while (!line_stream.eof()) {
-//                            getline(line_stream, word, '\t');
-//                            narr.push(i, stod(word));  // MPFR?!?!?! I don't know
-//                            ++i;
-//                        }
-//                    }
-//                    line_stream.clear();
-//                }
-//            }
-//            return narr;
-//
-//        } else {
-//            cerr << "Matrix parsing error:" << endl << "\tinput file [" << filepath << "] not found" << endl;
-//            return NamedVectorArray();
-//        }
-//
-//        // read first line with names
-//
-//        // put first element from each row to column names
-//
-//        // remove cell at the intersection of the first row and the first column
-//
-//        ifs.close();
-////        return NamedMatrix(colnames, rownames, Matrix<prob_t, Dynamic, Dynamic, ColMajor>(values.data()));
-//    }
-
 
 }
 

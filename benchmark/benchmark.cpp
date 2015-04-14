4005ce2da0e471ec80850595a91ff863bbfb7de7
Vadim Nazarov <vdn@mailbox.org>
2015-04-14 17:44:21 +0300

    add draft code for benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -5,11 +5,51 @@
 #ifndef _BENCHMARK_H_
 #define _BENCHMARK_H_
 
+#define BENCH_DATA_FOLDER string("/Users/vdn/ymir/benchmark/data/")
 
-//using namespace ymir;
+#include "parser.h"
+#include "statisticalinferencealgorithm.h"
+
+using namespace ymir;
 
 
 int main() {
+
+//    RepertoireParser parser;
+//    parser.loadConfig(BENCH_DATA_FOLDER + "../../parsers/mitcrdots.json");
+//
+//    VDJRecombinationGenes genes("Vgene",
+//                                BENCH_DATA_FOLDER + "trav.txt",
+//                                "Jgene",
+//                                BENCH_DATA_FOLDER + "traj.txt");
+//
+//    Cloneset cr;
+//    parser.parse(BENCH_DATA_FOLDER + "mitcr.alpha.300k.txt",
+//                 &cr,
+//                 genes,
+//                 RepertoireParser::AlignmentColumnOptions()
+//                         .setV(RepertoireParser::MAKE_IF_NOT_FOUND)
+//                         .setJ(RepertoireParser::MAKE_IF_NOT_FOUND)
+//                         .setD(RepertoireParser::SKIP));
+
+    RepertoireParser parser;
+    parser.loadConfig(BENCH_DATA_FOLDER + "../../parsers/mitcrdots.json");
+
+    VDJRecombinationGenes genes("Vgene",
+                                BENCH_DATA_FOLDER + "trbv.txt",
+                                "Jgene",
+                                BENCH_DATA_FOLDER + "trbj.txt",
+                                "Dgene",
+                                BENCH_DATA_FOLDER + "trbd.txt");
+
+    Cloneset cr;
+    parser.parse(BENCH_DATA_FOLDER + "mitcr.beta.200k.txt",
+                 &cr,
+                 genes,
+                 RepertoireParser::AlignmentColumnOptions()
+                         .setV(RepertoireParser::MAKE_IF_NOT_FOUND)
+                         .setJ(RepertoireParser::MAKE_IF_NOT_FOUND)
+                         .setD(RepertoireParser::OVERWRITE));
 
 //    Cloneset repertoire1K, repertoire10K, repertoire100K;
 //  With and without parallelisation

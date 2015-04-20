bbad5d2a1153f7061d98c3af6eaa9d3e5a5d9cde
Vadim Nazarov <vdn@mailbox.org>
2015-04-20 13:25:21 +0300

    zero length sequences in MarkovChain now returns probability 1

--- test/test.cpp
+++ test/test.cpp
@@ -907,11 +907,11 @@ YMIR_TEST_START(test_markovchain_nuc)
     string s = "ACGT";
 
     // .7 * .3 * .2 = .042
-    YMIR_ASSERT(mc.nucProbability("") == 0);
+    YMIR_ASSERT(mc.nucProbability("") == 1);
     YMIR_ASSERT(mc.nucProbability(s) == .042);
     YMIR_ASSERT(mc.nucProbability(s.begin(), 4) == .042);
     YMIR_ASSERT(mc.nucProbability(s.begin() + 1, 3) == .06);
-    YMIR_ASSERT(mc.nucProbability(s.begin(), 0) == 0);
+    YMIR_ASSERT(mc.nucProbability(s.begin(), 0) == 1);
 
     mc.updateProbabilities(vec.begin());
 

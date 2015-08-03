516708991ded6383220231497ea6e28ba2117d27
Vadim Nazarov <vdn@mailbox.org>
2015-08-03 18:05:57 +0300

    add a draft procedure for writing event prob tables

--- src/textdata.h
+++ src/textdata.h
@@ -5,7 +5,7 @@
 #ifndef YMIR_TEXTDATA_H
 #define YMIR_TEXTDATA_H
 
-#include <vector>
+//#include <vector>
 
 #include "types.h"
 
@@ -41,36 +41,54 @@ namespace ymir {
         virtual void addDataValue(prob_t value, size_t i = 0) { _data[i].push_back(value + _laplace); }
 
 
-        virtual void addDataVector(const vector<prob_t> &vec) {
-            _data.push_back(vector<prob_t>());
+        virtual void addDataVector(const std::vector<prob_t> &vec) {
+            _data.push_back(std::vector<prob_t>());
             for (auto i = 0; i < vec.size(); ++i) {
                 _data[_data.size() - 1].push_back(vec[i] + _laplace);
             }
+        }
+        
+        
+        virtual void addDataVector(std::vector<prob_t>::const_iterator start, std::vector<prob_t>::const_iterator end) {
+            _data.push_back(std::vector<prob_t>());
+            _data[_data.size() - 1].insert(_data[_data.size() - 1].cend(), start, end);
         }
 
 
         prob_t laplace() const { return _laplace; }
 
 
+        seg_index_t n_rows() const { return _rownames.size(); }
+
+
         seg_index_t n_columns() const { return _colnames.size(); }
 
 
-        void addRowName(const string& name) { _rownames.push_back(name); }
+        void addRowName(const std::string& name) { _rownames.push_back(name); }
 
 
-        void addColumnName(const string& name) { _colnames.push_back(name); }
+        void addColumnName(const std::string& name) { _colnames.push_back(name); }
 
 
-        const vector<string>& row_names() const { return _rownames; }
+        void addMetadata(seq_len_t data) { _metadata.push_back(data); }
 
 
-        const vector<string>& column_names() const { return _colnames; }
+        const std::vector<std::string>& row_names() const { return _rownames; }
 
 
-        virtual bool read(const string& filepath, string &err_message) = 0;
+        const std::vector<std::string>& column_names() const { return _colnames; }
 
 
-        const vector<prob_t>& data(size_t i) const { return _data[i]; }
+        seq_len_t metadata(size_t i) const { return _metadata[i]; }
+
+
+        virtual bool read(const std::string& filepath, std::string &err_message) = 0;
+
+
+        virtual bool write(const std::string& filepath) = 0;
+
+
+        const std::vector<prob_t>& data(size_t i) const { return _data[i]; }
 
 
         CONTAINER_TYPE type() const { return _type; }
@@ -78,21 +96,15 @@ namespace ymir {
 
         bool file_exists() const { return _file_exists; }
 
-
-        void addMetadata(seq_len_t data) { _metadata.push_back(data); }
-
-
-        seq_len_t metadata(size_t i) const { return _metadata[i]; }
-
     protected:
 
-        vector< vector<prob_t> > _data;
-        vector<string> _colnames;
-        vector<string> _rownames;
+        std::vector< std::vector<prob_t> > _data;
+        std::vector<std::string> _colnames;
+        std::vector<std::string> _rownames;
         prob_t _laplace;
         bool _skip_first_column, _file_exists;
         CONTAINER_TYPE _type;
-        vector<seq_len_t> _metadata;
+        std::vector<seq_len_t> _metadata;
 
 
         AbstractTDContainer() {
@@ -118,17 +130,18 @@ namespace ymir {
         virtual ~TDVector() { }
 
 
-        bool read(const string& filepath, string &err_message) {
+        bool read(const std::string& filepath, std::string &err_message) {
             ifstream ifs;
 
             ifs.open(filepath);
 
             if (ifs.is_open()) {
-                stringstream line_stream;
-                string line, word;
-                vector<prob_t> word_vec;
+                std::stringstream line_stream;
+                std::string line, word;
+                std::vector<prob_t> word_vec;
                 int line_num = 1;
                 bool skip_col_num_check = true, empty_line = false;
+
                 while (!ifs.eof()) {
                     getline(ifs, line);
                     if (line[0] != '\n' && line[0] != 0) {
@@ -139,7 +152,7 @@ namespace ymir {
                         if (skip_col_num_check) {
                             getline(line_stream, word, '\t');
                             this->addColumnName("Probability");
-                            _data.push_back(vector<prob_t>());
+                            _data.push_back(std::vector<prob_t>());
                             _data[0].reserve(60);
                         } else {
                             while (!line_stream.eof()) {
@@ -164,7 +177,7 @@ namespace ymir {
                                     _data[i].push_back(word_vec[i]);
                                 }
                             } else {
-                                stringstream ss;
+                                std::stringstream ss;
                                 ss << "ERROR: number of elements doesn't match the number of columns in the line " <<
                                 (int) line_num <<
                                 " (expected: " <<
@@ -193,6 +206,28 @@ namespace ymir {
             return false;
         }
 
+
+        bool write(const std::string& filepath) {
+            ofstream ofs;
+
+            ofs.open(filepath);
+
+            if (ofs.is_open()) {
+
+                for (auto i = 0; i < _data[0].size(); ++i) {
+                    std::cout << _data[0][i] << std::endl;
+                    ofs << _rownames[i] << "\t" << _data[0][i] << std::endl;
+                }
+
+                ofs.close();
+                _file_exists = true;
+                return true;
+            }
+
+            _file_exists = false;
+            return false;
+        }
+
     protected:
 
         TDVector() : AbstractTDContainer() {}
@@ -202,7 +237,7 @@ namespace ymir {
 
 
     /**
-     * \brief List of vectors for deletions and insertions.
+     * \brief List of std::vectors for deletions and insertions.
      */
     struct TDVectorList : public AbstractTDContainer {
     public:
@@ -213,15 +248,15 @@ namespace ymir {
         virtual ~TDVectorList() { }
 
 
-        bool read(const string& filepath, string &err_message) {
+        bool read(const std::string& filepath, std::string &err_message) {
             ifstream ifs;
 
             ifs.open(filepath);
 
             if (ifs.is_open()) {
-                stringstream line_stream;
-                string line, word;
-                vector<prob_t> word_vec;
+                std::stringstream line_stream;
+                std::string line, word;
+                std::vector<prob_t> word_vec;
                 int line_num = 1;
                 bool skip_col_num_check = true, empty_line = false;
                 while (!ifs.eof()) {
@@ -236,7 +271,7 @@ namespace ymir {
                                 getline(line_stream, word, '\t');
                                 if (i) {
                                     this->addColumnName(word);
-                                    _data.push_back(vector<prob_t>());
+                                    _data.push_back(std::vector<prob_t>());
                                     _data[_data.size() - 1].reserve(40);
                                 }
                                 ++i;
@@ -264,7 +299,7 @@ namespace ymir {
                                     _data[i].push_back(word_vec[i]);
                                 }
                             } else {
-                                stringstream ss;
+                                std::stringstream ss;
                                 ss << "ERROR: number of elements doesn't match the number of columns in the line " <<
                                 (int) line_num <<
                                 " (expected: " <<
@@ -293,6 +328,11 @@ namespace ymir {
             return false;
         }
 
+
+        bool write(const std::string& filepath) {
+
+        }
+
     protected:
 
         TDVectorList() : AbstractTDContainer() {}
@@ -309,24 +349,24 @@ namespace ymir {
         virtual ~TDMatrix() { }
 
 
-        virtual void addDataVector(const vector<prob_t> &vec) {
+        virtual void addDataVector(const std::vector<prob_t> &vec) {
             if (!_data.size()) {
-                _data.push_back(vector<prob_t>());
+                _data.push_back(std::vector<prob_t>());
             }
             _data[0].insert(_data[0].end(), vec.begin(), vec.end());
         }
 
 
-        bool read(const string& filepath, string &err_message) {
+        bool read(const std::string& filepath, std::string &err_message) {
             ifstream ifs;
 
             ifs.open(filepath);
 
             if (ifs.is_open()) {
-                _data.push_back(vector<prob_t>());
-                stringstream line_stream;
-                string line, word;
-                vector<prob_t> word_vec;
+                _data.push_back(std::vector<prob_t>());
+                std::stringstream line_stream;
+                std::string line, word;
+                std::vector<prob_t> word_vec;
                 int line_num = 1;
                 bool skip_col_num_check = true, empty_line = false;
                 while (!ifs.eof()) {
@@ -363,7 +403,7 @@ namespace ymir {
                             if (word_vec.size() == _colnames.size()) {
                                 _data[0].insert(_data[0].end(), word_vec.begin(), word_vec.end());
                             } else {
-                                stringstream ss;
+                                std::stringstream ss;
                                 ss << "ERROR: number of elements doesn't match the number of columns in the line " <<
                                 (int) line_num <<
                                 " (expected: " <<
@@ -392,6 +432,11 @@ namespace ymir {
             return false;
         }
 
+
+        bool write(const std::string& filepath) {
+
+        }
+
     protected:
 
         TDMatrix() : AbstractTDContainer() {}
@@ -414,18 +459,18 @@ namespace ymir {
         virtual ~TDMatrixList() { }
 
 
-        bool read(const string& filepath, string &err_message) {
+        bool read(const std::string& filepath, std::string &err_message) {
             ifstream ifs;
 
             ifs.open(filepath);
 
             if (ifs.is_open()) {
-                stringstream line_stream;
-                string line, word;
-                vector<prob_t> word_vec;
+                std::stringstream line_stream;
+                std::string line, word;
+                std::vector<prob_t> word_vec;
                 int line_num = 1, matrix_num = 0;
                 bool skip_col_num_check = true, in_matrix = true, empty_line = false;
-                _data.push_back(vector<prob_t>());
+                _data.push_back(std::vector<prob_t>());
                 while (!ifs.eof()) {
                     getline(ifs, line);
                     if (line[0] != '\n' && line[0] != 0) {
@@ -454,7 +499,7 @@ namespace ymir {
                         line_stream.clear();
                     } else {
                         matrix_num++;
-                        _data.push_back(vector<prob_t>());
+                        _data.push_back(std::vector<prob_t>());
                         empty_line = true;
                         skip_col_num_check = true;
                     }
@@ -464,7 +509,7 @@ namespace ymir {
                             if (word_vec.size() == _metadata[matrix_num]) {
                                 _data[matrix_num].insert(_data[matrix_num].end(), word_vec.begin(), word_vec.end());
                             } else {
-                                stringstream ss;
+                                std::stringstream ss;
                                 ss << "ERROR: number of elements doesn't match the number of columns in the line " <<
                                 (int) line_num <<
                                 " (expected: " <<
@@ -493,6 +538,11 @@ namespace ymir {
             return false;
         }
 
+
+        bool write(const std::string& filepath) {
+
+        }
+
     protected:
 
         TDMatrixList() : AbstractTDContainer() {}
@@ -505,20 +555,20 @@ namespace ymir {
      *
      * \brief Read to the container data from the file with event probabilities.
      */
-    AbstractTDContainer* read_textdata(const string& filepath,
-                                       const string& filetype,
+    AbstractTDContainer* read_textdata(const std::string& filepath,
+                                       const std::string& filetype,
                                        bool skip_first_column,
                                        prob_t laplace,
-                                       string& err_message) {
+                                       std::string& err_message) {
         AbstractTDContainer* container;
         err_message = "OK";
         if (filetype == "matrix") {
             container = new TDMatrix(skip_first_column, laplace);
-        } else if (filetype == "vector.list") {
+        } else if (filetype == "std::vector.list") {
             container = new TDVectorList(skip_first_column, laplace);
         } else if (filetype == "matrix.list") {
             container = new TDMatrixList(skip_first_column, laplace);
-        } else if (filetype == "vector") {
+        } else if (filetype == "std::vector") {
             container = new TDVector(skip_first_column, laplace);
         } else {
             if (filetype == "") {

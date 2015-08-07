898e53c5424487e158b0625f842ceed1eb340864
Vadim Nazarov <vdn@mailbox.org>
2015-08-07 16:33:52 +0300

    add a bit more of C++ code for computing probs

--- scripts/compute.cpp
+++ scripts/compute.cpp
@@ -20,21 +20,29 @@ using namespace ymir;
 /**
  * \brief Main function of a script for computing generation probabitilies. It has
  * a strict order of input arguments in console:
- * argv[0] - name of the script (default)
- * argv[1] - path to an input file
- * argv[2] - path to a model
- * argv[3] - path to an output file
+ * argv[0] - name of the script (default);
+ * argv[1] - path to an input file;
+ * argv[2] - path to a model;
+ * argv[3] - path to an output file;
+ * argv[4] - 0 if model should use stored gene usage; 1 if model should recompute gene usage from the input file.
  */
 int main(int argc, char* argv[]) {
     std::string in_file_path(argv[1]), model_path(argv[2]), out_file_path(argv[3]);
+    bool recompute_genes = std::stoi(argv[4]);
 
     ProbabilisticAssemblingModel model(model_path);
 
     RepertoireParser parser;
     Cloneset cloneset;
-    parser.parse(in_file_path, &cloneset, model.gene_segments());  // ???
+    parser.parse(in_file_path, &cloneset, model.gene_segments(),
+                 RepertoireParser::AlignmentColumnOptions()
+                         .setV(RepertoireParser::MAKE_IF_NOT_FOUND)
+                         .setJ(RepertoireParser::MAKE_IF_NOT_FOUND)
+                         .setD(RepertoireParser::OVERWRITE));
 
-    model.updateGeneUsage(cloneset);
+    if (recompute_genes) {
+        model.updateGeneUsage(cloneset);
+    }
     auto prob_vec = model.computeFullProbabilities(cloneset);
 
     std::ofstream ofs;

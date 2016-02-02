009915abe066094e72155dd082f4cb7f765b0084
Vadim Nazarov <vdn@mailbox.org>
2016-02-02 17:03:36 +0300

    add an interface to gene alignments

--- Ymir/src/alignment.h
+++ Ymir/src/alignment.h
@@ -396,23 +396,74 @@ namespace ymir {
          * \brief Get alignments for the specific gene.
          */
         ///@{
+        seq_len_t getVarGeneStart(seg_index_t vgene) const { 
+            return _alignments.pattern_start(vgene);
+        }
+
+        seq_len_t getVarSeqStart() const {
+            return _alignments.text_start(vgene);
+        }
+
+        seq_len_t getVarLen() const {
+            return _alignments.len(vgene);
+        }
+
+        bool isVarMismatch() const {
+            return _alignments.isMismatch(vgene);
+        }
+
+
+        seq_len_t getJoiGeneStart(seg_index_t jgene) const { 
+            return _alignments.pattern_start(_segments[0] + jgene);
+        }
+
+        seq_len_t getJoiSeqStart() const {
+            return _alignments.text_start(_segments[0] + jgene);
+        }
+
+        seq_len_t getJoiLen() const {
+            return _alignments.len(_segments[0] + jgene);
+        }
+
+        bool isJoiMismatch() const {
+            return _alignments.isMismatch(_segments[0] + jgene);
+        }
+
+
+        seq_len_t getDivGeneStart(seg_index_t dgene, seg_index_t align_i) const { 
+            return _alignments.pattern_start(_segments[0] + _segments[1] + _n_D_alignments[dgene] + align_i);
+        }
+
+        seq_len_t getDivSeqStart(seg_index_t dgene, seg_index_t align_i) const {
+            return _alignments.text_start(_segments[0] + _segments[1] + _n_D_alignments[dgene] + align_i);
+        }
+
+        seq_len_t getDivLen(seg_index_t dgene, seg_index_t align_i) const {
+            return _alignments.len(_segments[0] + _segments[1] + _n_D_alignments[dgene] + align_i);
+        }
+
+        bool isDivMismatch(seg_index_t dgene, seg_index_t align_i) const {
+            return _alignments.isMismatch(_segments[0] + _segments[1] + _n_D_alignments[dgene] + align_i);
+        }
         ///@}
 
 
+        /**
+         * \brief Get the number of D gene alignments for the given D gene.
+         */
         seq_len_t numDivAlignments(seg_index_t index) const {
-            if (_n_D_alignments) {
-                return _n_D_alignments[index];
-            } else {
-                return 0;
-            }
+            return _n_D_alignments[index + 1] - _n_D_alignments[index];
         }
+
 
     protected:
 
         unique_ptr<seg_index_t[]> _segments;  /// Two concatenated vectors: vector of length 3 w/ numbers of aligned segments (V-J-D) and
                                               /// vector of indices of segments, aligned on this clone: V1--V2--V3--J1--J2--D1--D2--...
         NoGapAlignmentVector _alignments;  /// Vector of alignments for segments.
-        unique_ptr<seq_len_t[]> _n_D_alignments;  /// Number of alignments for each D gene segment; vector's length == _segments[2]
+        unique_ptr<seq_len_t[]> _n_D_alignments;  /// Accumulated number of alignments for each D gene segment;
+                                                  /// vector's length == _segments[2] + 1. Number of alignments for i-th
+                                                  /// gene is equal to v[i+1] - v[i].
 
 
         VDJAlignment();

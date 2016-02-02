4b0ac4473d79205c9d69d645dd115719e444b2c7
Vadim Nazarov <vdn@mailbox.org>
2016-02-02 13:06:31 +0300

    refactor NaiveCDR3Aligner

--- Ymir/src/aligner.h
+++ Ymir/src/aligner.h
@@ -36,11 +36,11 @@ namespace ymir {
 
 
     /**
-     * \class AbstractVDJAligner
+     * \class VDJAlignerBase
      */
     // template <typename V_Aligner<typename Alignment>, typename D_Aligner<typename Alignment>, typename J_Aligner<typename Alignment>>
     template <typename V_Aligner, typename D_Aligner, typename J_Aligner>
-    class AbstractVDJAligner {
+    class VDJAlignerBase {
     public:
 
         /**
@@ -97,13 +97,13 @@ namespace ymir {
         };
 
 
-        AbstractVDJAligner() { }
+        VDJAlignerBase() { }
 
 
         /**
          *
          */
-        AbstractVDJAligner(const VDJRecombinationGenes &genes,
+        VDJAlignerBase(const VDJRecombinationGenes &genes,
                            alignment_score_t threshold,
                            const AlignmentEventScore &v_score = AlignmentEventScore(1, -1, -1, -1),
                            const AlignmentEventScore &d_score = AlignmentEventScore(1, -1, -1, -1),
@@ -113,7 +113,7 @@ namespace ymir {
         }
 
 
-        ~AbstractVDJAligner() {
+        ~VDJAlignerBase() {
         }
 
 
@@ -279,115 +279,6 @@ namespace ymir {
     };
 
 
-//    class NaiveNucleotideAligner : public AbstractAligner<string, seq_len_t> {
-//    class NaiveNucleotideAligner : public AbstractVDJAligner {
-    class NaiveNucleotideAligner : public AbstractAligner {
-    public:
-
-        virtual seq_len_t align5end(const string& pattern, const string& text) const {
-            // seq_len_t p_size = pattern.size(), t_size = text.size(), matches = 0;
-            // for (seq_len_t i = 0; i < min(p_size, t_size); ++i) {
-            //     if (pattern[i] != text[i]) { break; }
-            //     matches += 1;
-            // }
-            // return matches;
-        }
-
-
-        virtual seq_len_t align3end(const string& pattern, const string& text) const {
-            // seq_len_t p_size = pattern.size(), t_size = text.size(), matches = 0;
-            // for (seq_len_t i = 0; i < min(p_size, t_size); ++i) {
-            //     if (pattern[p_size - i - 1] != text[t_size - i - 1]) { break; }
-            //     matches += 1;
-            // }
-            // return matches;
-        }
-
-
-        virtual LocalAlignmentIndices alignLocal(const string& pattern, const string& text, seq_len_t match_min_len = 3) const {
-            vector<seq_len_t> vec;
-            vec.reserve(DEFAULT_LOCAL_ALIGNMENT_RESERVE);
-
-            seq_len_t t_size = text.size(), p_size = pattern.size(), min_size = min(t_size, p_size), min_subsize;
-            bool open_match;
-            seq_len_t p_start, t_start;
-
-            for (seq_len_t pattern_i = 1 /* WTF?! */ ; pattern_i < p_size - match_min_len + 1; ++pattern_i) {
-                open_match = false;
-                min_subsize = min(p_size - pattern_i, (int) t_size);
-                for (seq_len_t i = 0; i < min_subsize; ++i) {
-                    if (pattern[pattern_i + i] == text[i]) {
-                        if (!open_match) {
-                            p_start = pattern_i + i;
-                            t_start = i;
-                            open_match = true;
-                        }
-                    } else if (open_match) {
-                        if ((pattern_i + i - p_start) >= match_min_len) {
-//                            vec.push_back(p_start);
-//                            vec.push_back(pattern_i + i - 1);
-//                            vec.push_back(t_start);
-//                            vec.push_back(i - 1);
-                            vec.push_back(p_start + 1);
-                            vec.push_back(t_start + 1);
-                            vec.push_back(pattern_i + i - p_start);
-                        }
-                        open_match = false;
-                    }
-                }
-                if (open_match && (pattern_i + min_subsize - p_start) >= match_min_len) {
-//                    vec.push_back(p_start);
-//                    vec.push_back(pattern_i + min_subsize - 1);
-//                    vec.push_back(t_start);
-//                    vec.push_back(min_subsize - 1);
-                    vec.push_back(p_start + 1);
-                    vec.push_back(t_start + 1);
-                    vec.push_back(pattern_i + min_subsize - p_start);
-                }
-            }
-
-            for (seq_len_t text_i = 0; text_i < t_size - match_min_len + 1; ++text_i) {
-                open_match = false;
-                min_subsize = min((int) p_size, t_size - text_i);
-                for (seq_len_t i = 0; i < min_subsize; ++i) {
-                    if (pattern[i] == text[text_i + i]) {
-                        if (!open_match) {
-                            p_start = i;
-                            t_start = text_i + i;
-                            open_match = true;
-                        }
-                    } else if (open_match) {
-                        if ((i - p_start) >= match_min_len) {
-//                            vec.push_back(p_start);
-//                            vec.push_back(i - 1);
-//                            vec.push_back(t_start);
-//                            vec.push_back(text_i + i - 1);
-                            vec.push_back(p_start + 1);
-                            vec.push_back(t_start + 1);
-                            vec.push_back(i - p_start);
-                        }
-                        open_match = false;
-                    }
-                }
-                if (open_match && (min_subsize - p_start) >= match_min_len) {
-//                    vec.push_back(p_start);
-//                    vec.push_back(min_subsize - 1);
-//                    vec.push_back(t_start);
-//                    vec.push_back(text_i + min_subsize - 1);
-                    vec.push_back(p_start + 1);
-                    vec.push_back(t_start + 1);
-                    vec.push_back(min_subsize - p_start);
-                }
-            }
-
-//            for (size_t i = 0; i < vec.size(); ++i) { ++vec[i]; }
-            return LocalAlignmentIndices(vec);
-        }
-
-    };
-
-
-//    class NaiveAminoAcidAligner : public AbstractAligner<string, seq_len_t> {
     class NaiveAminoAcidAligner : public AbstractAligner {
 
     public:
@@ -486,36 +377,86 @@ namespace ymir {
      */
     ///@{
     struct NaiveCDR3AlignerFunctor_V {
-        NoGapAlignment operator()(const sequence_t &pattern, const sequence_t &text) const {
+        void operator()(const sequence_t &pattern, const sequence_t &text, NoGapAlignmentVector *avec) const {
             seq_len_t p_size = pattern.size(), t_size = text.size(), matches = 0;
+
             for (seq_len_t i = 0; i < min(p_size, t_size); ++i) {
                 if (pattern[i] != text[i]) { break; }
                 matches += 1;
             }
-            return NoGapAlignment(0, 0, matches);
+
+            avec->addAlignment(1, 1, matches);
         }
     };
 
     struct NaiveCDR3AlignerFunctor_D {
-        NoGapAlignment operator()(const sequence_t &pattern, const sequence_t &text) const {
-            return NoGapAlignment();
+        void operator()(const sequence_t &pattern, const sequence_t &text, NoGapAlignmentVector *avec) const {
+            seq_len_t t_size = text.size(), p_size = pattern.size(), min_size = min(t_size, p_size), min_subsize;
+            bool open_match;
+            seq_len_t p_start, t_start;
+
+            for (seq_len_t pattern_i = 1 /* WTF?! */ ; pattern_i < p_size - match_min_len + 1; ++pattern_i) {
+                open_match = false;
+                min_subsize = min(p_size - pattern_i, (int) t_size);
+                for (seq_len_t i = 0; i < min_subsize; ++i) {
+                    if (pattern[pattern_i + i] == text[i]) {
+                        if (!open_match) {
+                            p_start = pattern_i + i;
+                            t_start = i;
+                            open_match = true;
+                        }
+                    } else if (open_match) {
+                        if ((pattern_i + i - p_start) >= match_min_len) {
+                            avec->addAlignment(p_start + 1, t_start + 1, pattern_i + i - p_start);
+                        }
+                        open_match = false;
+                    }
+                }
+                if (open_match && (pattern_i + min_subsize - p_start) >= match_min_len) {
+                    avec->addAlignment(p_start + 1, t_start + 1, pattern_i + min_subsize - p_start);
+                }
+            }
+
+            for (seq_len_t text_i = 0; text_i < t_size - match_min_len + 1; ++text_i) {
+                open_match = false;
+                min_subsize = min((int) p_size, t_size - text_i);
+                for (seq_len_t i = 0; i < min_subsize; ++i) {
+                    if (pattern[i] == text[text_i + i]) {
+                        if (!open_match) {
+                            p_start = i;
+                            t_start = text_i + i;
+                            open_match = true;
+                        }
+                    } else if (open_match) {
+                        if ((i - p_start) >= match_min_len) {
+                            avec->addAlignment(p_start + 1, t_start + 1, i - p_start);
+                        }
+                        open_match = false;
+                    }
+                }
+                if (open_match && (min_subsize - p_start) >= match_min_len) {
+                    avec->addAlignment(p_start + 1, t_start + 1, min_subsize - p_start);
+                }
+            }
         }
     };
 
     struct NaiveCDR3AlignerFunctor_J {
-        NoGapAlignment operator()(const sequence_t &pattern, const sequence_t &text) const {
+        void operator()(const sequence_t &pattern, const sequence_t &text, NoGapAlignmentVector *avec) const {
             seq_len_t p_size = pattern.size(), t_size = text.size(), matches = 0;
+
             for (seq_len_t i = 0; i < min(p_size, t_size); ++i) {
                 if (pattern[p_size - i - 1] != text[t_size - i - 1]) { break; }
                 matches += 1;
             }
-            return NoGapAlignment(0, t_size - matches + 1, matches);
+
+            avec->addAlignment(1, t_size - matches + 1, matches);
         }
     };
     ///@}
 
 
-    typedef AbstractVDJAligner<NaiveCDR3AlignerFunctor_V, NaiveCDR3AlignerFunctor_D, NaiveCDR3AlignerFunctor_J> NaiveCDR3NucleotideAligner;
+    // typedef VDJAlignerBase<NaiveCDR3AlignerFunctor_V, NaiveCDR3AlignerFunctor_D, NaiveCDR3AlignerFunctor_J> NaiveCDR3NucleotideAligner;
 
 
     //
@@ -528,10 +469,11 @@ namespace ymir {
      */
     ///@{
     struct CDR3AlignerFunctor_V {
-        NoGapAlignment operator()(const sequence_t &pattern, const sequence_t &text) const {
+        void operator()(const sequence_t &pattern, const sequence_t &text, NoGapAlignmentVector *avec) const {
             seq_len_t p_size = pattern.size(), t_size = text.size();
-            NoGapAlignment::mismatch_storage_t vec;
+            NoGapAlignment::events_storage_t vec;
             vec.reserve(min(p_size, t_size));
+
             for (seq_len_t i = 0; i < min(p_size, t_size); ++i) {
                 if (pattern[i] == text[i]) { 
                     vec.push_back(false);
@@ -539,21 +481,23 @@ namespace ymir {
                     vec.push_back(true);
                 }
             }
-            return NoGapAlignment(0, 0, vec);
+
+            avec->addAlignment(1, 1, vec);
         }
     };
 
     struct CDR3AlignerFunctor_D {
-        NoGapAlignment operator()(const sequence_t &pattern, const sequence_t &text) const {
-            return NoGapAlignment();
+        void operator()(const sequence_t &pattern, const sequence_t &text, NoGapAlignmentVector *avec) const {
+            // pass
         }
     };
 
     struct CDR3AlignerFunctor_J {
-        NoGapAlignment operator()(const sequence_t &pattern, const sequence_t &text) const {
+        void operator()(const sequence_t &pattern, const sequence_t &text, NoGapAlignmentVector *avec) const {
             seq_len_t p_size = pattern.size(), t_size = text.size();
-            NoGapAlignment::mismatch_storage_t vec;
+            NoGapAlignment::events_storage_t vec;
             vec.reserve(min(p_size, t_size));
+
             for (seq_len_t i = 0; i < min(p_size, t_size); ++i) {
                 if (pattern[p_size - i - 1] != text[t_size - i - 1]) {
                     vec.push_back(false);
@@ -561,13 +505,14 @@ namespace ymir {
                     vec.push_back(true);
                 }
             }
-            return NoGapAlignment(0, t_size - matches + 1, vec);
+
+            avec->addAlignment(1, t_size - matches + 1, vec);
         }
     };
     ///@}
 
 
-    typedef AbstractVDJAligner<CDR3AlignerFunctor_V, CDR3AlignerFunctor_D, CDR3AlignerFunctor_J> CDR3NucleotideAligner;
+    // typedef VDJAlignerBase<CDR3AlignerFunctor_V, CDR3AlignerFunctor_D, CDR3AlignerFunctor_J> CDR3NucleotideAligner;
 
 
     /**
@@ -635,7 +580,7 @@ namespace ymir {
      *
      * \brief Smith-Waterman aligner for finding maximal matches with gaps. Don't takes into account errors.
      */
-    typedef AbstractVDJAligner<SWAlignerFunctor_VJ, SWAlignerFunctor_D, SWAlignerFunctor_VJ> SmithWatermanAligner;
+    // typedef VDJAlignerBase<SWAlignerFunctor_VJ, SWAlignerFunctor_D, SWAlignerFunctor_VJ> SmithWatermanAligner;
 
 
     //
@@ -665,7 +610,7 @@ namespace ymir {
      *
      * \brief Smith-Waterman aligner without gaps, returns maximal matches with information about mismatch errors.
      */
-    typedef AbstractVDJAligner<SWNGAlignerFunctor_VJ, SWNGAlignerFunctor_D, SWNGAlignerFunctor_VJ> SmithWatermanNoGapAligner;
+    // typedef VDJAlignerBase<SWNGAlignerFunctor_VJ, SWNGAlignerFunctor_D, SWNGAlignerFunctor_VJ> SmithWatermanNoGapAligner;
 
 }
 

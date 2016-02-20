db454cf90e04d7d0f17686bf8392535fdda447c7
Vadim Nazarov <vdn@mailbox.org>
2016-02-20 14:44:49 +0300

    refactor Clonotype so it's now infherited from VDJAlignment

--- Ymir/src/clonotype.h
+++ Ymir/src/clonotype.h
@@ -29,7 +29,7 @@
 
 
 namespace ymir {
-    
+
 
     struct Clonotype;
 
@@ -43,172 +43,36 @@ namespace ymir {
     /**
     * \struct Clonotype
     */
-    // struct Clonotype : public VDJAlignment {
-    struct Clonotype {
+    struct Clonotype : public VDJAlignment {
 
-        Clonotype(const std::string& sequence,
+        Clonotype(const sequence_t &sequence,
                   SequenceType seq_type,
                   Recombination recomb,
-                  seg_index_t *segments,
-                  seq_len_t *alignments,
-                  seq_len_t *n_D_alignments = nullptr)
-                : _sequence(sequence),
+                  const segments_storage_t &segments,
+                  const NoGapAlignmentVector &alignments,
+                  const n_D_alignments_storage_t &n_D_alignments)
+                : VDJAlignment(segments, alignments, n_D_alignments),
+                  _sequence(sequence),
                   _seq_type(seq_type),
-                  _recomb(recomb),
-                  _segments(segments),
-                  _alignments(alignments),
-                  _n_D_alignments(n_D_alignments)
-        { }
-
-
-        Clonotype(const Clonotype& other)
-                : _seq_type(other._seq_type),
-                  _sequence(other._sequence),
-                  _recomb(other._recomb)
+                  _recomb(recomb)
         {
-            _segments = new seg_index_t[3 + other._segments[0] + other._segments[1] + other._segments[2]];
-            _segments[0] = other._segments[0];
-            _segments[1] = other._segments[1];
-            _segments[2] = other._segments[2];
-            for (int i = 0; i < _segments[0] + _segments[1] + _segments[2]; ++i) {
-                _segments[i + 3] = other._segments[i + 3];
-            }
-
-            int sum_align = 3*_segments[0] + 3*_segments[1];
-            if (_segments[2]) {
-                _n_D_alignments = new seq_len_t[_segments[2]];
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    _n_D_alignments[i] = other._n_D_alignments[i];
-                }
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    sum_align += _n_D_alignments[i] * 3;
-                }
-
-            } else {
-                _n_D_alignments = nullptr;
-            }
-
-            _alignments = new seq_len_t[sum_align];
-            for (int i = 0; i < sum_align; ++i) {
-                _alignments[i] = other._alignments[i];
-            }
         }
 
 
-        Clonotype(const ClonotypePtr& other)
-                : _seq_type(other->_seq_type),
-                  _sequence(other->_sequence),
-                  _recomb(other->_recomb)
+        Clonotype(const sequence_t &sequence,
+                  SequenceType seq_type,
+                  Recombination recomb,
+                  VDJAlignment alignment)
+                : VDJAlignment(alignment),
+                  _sequence(sequence),
+                  _seq_type(seq_type),
+                  _recomb(recomb)
         {
-            _segments = new seg_index_t[3 + other->_segments[0] + other->_segments[1] + other->_segments[2]];
-            _segments[0] = other->_segments[0];
-            _segments[1] = other->_segments[1];
-            _segments[2] = other->_segments[2];
-            for (int i = 0; i < _segments[0] + _segments[1] + _segments[2]; ++i) {
-                _segments[i + 3] = other->_segments[i + 3];
-            }
-
-            int sum_align = 3*_segments[0] + 3*_segments[1];
-            if (_segments[2]) {
-                _n_D_alignments = new seq_len_t[_segments[2]];
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    _n_D_alignments[i] = other->_n_D_alignments[i];
-                }
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    sum_align += _n_D_alignments[i] * 3;
-                }
-
-            } else {
-                _n_D_alignments = nullptr;
-            }
-
-            _alignments = new seq_len_t[sum_align];
-            for (int i = 0; i < sum_align; ++i) {
-                _alignments[i] = other->_alignments[i];
-            }
         }
 
 
-        ///@{
-        Clonotype& operator=(const Clonotype &other) {
-            _seq_type = other._seq_type;
-            _sequence = other._sequence;
-            _recomb = other._recomb;
-
-            _segments = new seg_index_t[3 + other._segments[0] + other._segments[1] + other._segments[2]];
-            _segments[0] = other._segments[0];
-            _segments[1] = other._segments[1];
-            _segments[2] = other._segments[2];
-            for (int i = 0; i < _segments[0] + _segments[1] + _segments[2]; ++i) {
-                _segments[i + 3] = other._segments[i + 3];
-            }
-
-            int sum_align = 3*_segments[0] + 3*_segments[1];
-            if (_segments[2]) {
-                _n_D_alignments = new seq_len_t[_segments[2]];
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    _n_D_alignments[i] = other._n_D_alignments[i];
-                }
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    sum_align += _n_D_alignments[i] * 3;
-                }
-
-            } else {
-                _n_D_alignments = nullptr;
-            }
-
-            _alignments = new seq_len_t[sum_align];
-            for (int i = 0; i < sum_align; ++i) {
-                _alignments[i] = other._alignments[i];
-            }
-        }
-
-        Clonotype& operator=(const ClonotypePtr &other) {
-            _seq_type = other->_seq_type;
-
-            _sequence = other->_sequence;
-            _recomb = other->_recomb;
-
-            _segments = new seg_index_t[3 + other->_segments[0] + other->_segments[1] + other->_segments[2]];
-            _segments[0] = other->_segments[0];
-            _segments[1] = other->_segments[1];
-            _segments[2] = other->_segments[2];
-            for (int i = 0; i < _segments[0] + _segments[1] + _segments[2]; ++i) {
-                _segments[i + 3] = other->_segments[i + 3];
-            }
-
-            int sum_align = 3*_segments[0] + 3*_segments[1];
-            if (_segments[2]) {
-                _n_D_alignments = new seq_len_t[_segments[2]];
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    _n_D_alignments[i] = other->_n_D_alignments[i];
-                }
-                for  (int i = 0; i < _segments[2]; ++i) {
-                    sum_align += _n_D_alignments[i] * 3;
-                }
-
-            } else {
-                _n_D_alignments = nullptr;
-            }
-
-            _alignments = new seq_len_t[sum_align];
-            for (int i = 0; i < sum_align; ++i) {
-                _alignments[i] = other->_alignments[i];
-            }
-        }
-        ///@}
-
-
-        virtual ~Clonotype() {
-            if (_segments) {
-                delete [] _segments;
-            }
-            if (_alignments) {
-                delete [] _alignments;
-            }
-            if (_n_D_alignments) {
-                delete [] _n_D_alignments;
-            }
+        virtual ~Clonotype()
+        {
         }
 
 
@@ -225,7 +89,7 @@ namespace ymir {
             return _sequence;
         }
 
-        const sequence_t& aa_sequence() const {
+        sequence_t aa_sequence() const {
 #ifndef DNDEBUG
             check_and_throw(_seq_type == UNDEF_SEQ_TYPE, "Clonotype's call to aa_sequence() is incorrect: undefined sequence type.");
 #endif
@@ -246,68 +110,13 @@ namespace ymir {
 
         SequenceType sequence_type() const { return _seq_type; }
 
-
-        /** Get index of aligned gene segment for specific gene. */
-        ///@{
-        seg_index_t getVar(size_t index) const { return _segments[3 + index]; }
-
-        seg_index_t getJoi(size_t index) const { return _segments[3 + _segments[0] + index]; }
-
-        seg_index_t getDiv(size_t index) const { return _segments[3 + _segments[0] + _segments[1] + index]; }
-        ///@}
-
-
-        /** Get number of alignments for specific gene. */
-        ///@{
-        seg_index_t nVar() const { return _segments[0]; }
-
-        seg_index_t nJoi() const { return _segments[1]; }
-
-        seg_index_t nDiv() const { return _segments[2]; }
-        ///@}
-
-
-        ///@{
-        Alignment getVarAlignment(seg_index_t gene_index) const { return Alignment(_alignments + 3*gene_index); }
-
-        Alignment getJoiAlignment(seg_index_t gene_index) const { return Alignment(_alignments + 3*_segments[0] + 3*gene_index); }
-
-        Alignment getDivAlignment(seg_index_t gene_index, seg_index_t alignment_index) const {
-            seq_len_t shift = 3*_segments[0] + 3*_segments[1];
-            for (size_t i = 0; i < gene_index; ++i) {
-                shift += 3 * _n_D_alignments[i];
-            }
-            return Alignment(_alignments + shift + 3*alignment_index);
-        }
-        ///@}
-
-
-        seq_len_t numDivAlignments(seg_index_t index) const {
-            if (_n_D_alignments) {
-                return _n_D_alignments[index];
-            } else {
-                return 0;
-            }
-        }
-
     protected:
 
         Recombination _recomb;
 
         SequenceType _seq_type;
 
-        std::string _sequence; //* CDR3 or full nucleotide or amino acid sequence of a clone. */
-
-        seg_index_t *_segments; /// Two concatenated vectors: vector of length 3 w/ numbers of aligned segments (V-J-D) and
-                                /// vector of indices of segments, aligned on this clone: V1--V2--V3--J1--J2--D1--D2--...
-
-        seq_len_t *_alignments;  /// Vector of 3-tuples 1-based alignments (gene segment start, reference sequence start, alignment length)
-                                 /// for alignments for each aligned gene segment.
-
-        seq_len_t *_n_D_alignments; //* Number of aligned D gene segments (i.e., 3-tuples); vector's length == _segments[2] */
-
-
-        // NoGapAlignmentVector _alignments;
+        sequence_t _sequence; //* CDR3 or full nucleotide or amino acid sequence of a clone. */
 
 
         /**

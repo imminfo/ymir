6ce82faa5a64371e879f93bf6697f3340885e87d
Vadim Nazarov <vdn@mailbox.org>
2016-03-04 15:09:06 +0300

    add space

--- ytest.py
+++ ytest.py
@@ -3,4 +3,4 @@ import os
 if __name__ == "__main__":
     for f in os.listdir("./build/test/"):
         if f.find("Test_") != -1:
-            os.system(f + os.getcwd() + '/test/data/')
+            os.system(f + " " + os.getcwd() + '/test/data/')

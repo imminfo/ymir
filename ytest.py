3ce61763e4f77d5b71c5beab2aa1d58688973783
Vadim Nazarov <vdn@mailbox.org>
2016-03-04 15:43:05 +0300

    minor change

--- ytest.py
+++ ytest.py
@@ -3,4 +3,9 @@ import os
 if __name__ == "__main__":
     for f in os.listdir("./build/test/"):
         if f.find("Test_") != -1:
+            print("==================================")
+            print("Run test cases from ", f)
+            print("==================================")
+            print()
             os.system("./build/test/" + f + " " + os.getcwd() + '/test/data/')
+            print()

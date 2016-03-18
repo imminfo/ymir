8cb0ef1a0e3770268b22e575b8fa38cd5379b6e8
Vadim Nazarov <vdn@mailbox.org>
2016-03-18 22:24:49 +0300

    debug

--- benchmark/bench_inference.cpp
+++ benchmark/bench_inference.cpp
@@ -41,10 +41,10 @@ int main(int argc, char* argv[]) {
     //
     // TCR alpha chain repertoire - VJ recombination
     //
-    VDJRecombinationGenes vj_single_genes("Vgene",
-                                          "/home/vadim/ymir/benchmark/data/trav.txt",
-                                          "Jgene",
-                                          "/home/vadim/ymir/benchmark/data/traj.txt");
+    VDJRecombinationGenes vj_single_genes(string("Vgene"),
+                                          string("/home/vadim/ymir/benchmark/data/trav.txt"),
+                                          string("Jgene"),
+                                          string("/home/vadim/ymir/benchmark/data/traj.txt"));
 
 
     // VDJRecombinationGenes vj_single_genes2(vj_single_genes);

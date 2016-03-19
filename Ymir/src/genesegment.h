6790686975547fe3370a9b4dacac73007045ff9e
Vadim Nazarov <vdn@mailbox.org>
2016-03-19 19:25:21 +0300

    debug

--- Ymir/src/genesegment.h
+++ Ymir/src/genesegment.h
@@ -333,25 +333,25 @@ namespace ymir {
                 _D.reset(new GeneSegmentAlphabet(other.D()));
                 std::cout << "Copied " << std::to_string(_D->size()) << " D alleles." << std::endl;
             } else {
-                std::cout << "No D supplied." << std::endl;
+                std::cout << "No D copied." << std::endl;
             }
        }
 
 
        VDJRecombinationGenes& operator=(const VDJRecombinationGenes &other) {
             _V.reset(new GeneSegmentAlphabet(other.V()));
-            // _J.reset(new GeneSegmentAlphabet(other.J()));
+            _J.reset(new GeneSegmentAlphabet(other.J()));
 
-            // std::cout << "Assigned " << std::to_string(_V->size()) << " V alleles." << std::endl;
-            // std::cout << "Assigned " << std::to_string(_J->size()) << " J alleles." << std::endl;
+            std::cout << "Assigned " << std::to_string(_V->size()) << " V alleles." << std::endl;
+            std::cout << "Assigned " << std::to_string(_J->size()) << " J alleles." << std::endl;
 
-            // if (other._D) {
-            //     _D.reset(new GeneSegmentAlphabet(other.D()));
-            //     std::cout << "Assigned " << std::to_string(_D->size()) << " D alleles." << std::endl;
-            // } else {
-            //     _D.release();
-            //     std::cout << "Assigned 0 D alleles." << std::endl;
-            // }
+            if (other._D) {
+                _D.reset(new GeneSegmentAlphabet(other.D()));
+                std::cout << "Assigned " << std::to_string(_D->size()) << " D alleles." << std::endl;
+            } else {
+                _D.release();
+                std::cout << "No D alleles assigned." << std::endl;
+            }
 
             return *this;
        }

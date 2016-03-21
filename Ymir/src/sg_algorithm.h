2315fb204e7f6cb2ac55d577e93c307f1cb81cdb
Vadim Nazarov <vdn@mailbox.org>
2016-03-21 12:08:14 +0300

    debug

--- Ymir/src/sg_algorithm.h
+++ Ymir/src/sg_algorithm.h
@@ -201,22 +201,64 @@ namespace ymir {
                 changed[new_param_vec.event_index(VJ_VAR_JOI_INS_NUC, 0, 2)] = true;
                 changed[new_param_vec.event_index(VJ_VAR_JOI_INS_NUC, 0, 3)] = true;
             } else {
-                int k = 0;
-                for (auto prev_nuc = 0; prev_nuc < 4; ++prev_nuc) {
-                    for (auto next_nuc = 0; next_nuc < 4; ++next_nuc, ++k) {
-                        new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, prev_nuc, next_nuc)] += fb.VD_nuc_probs()[k];
-                        changed[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, prev_nuc, next_nuc)] = true;
-                    }
-                }
+                // Because optimization.
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 0, 0)] += fb.VD_nuc_probs()[0];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 0, 1)] += fb.VD_nuc_probs()[1];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 0, 2)] += fb.VD_nuc_probs()[2];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 0, 3)] += fb.VD_nuc_probs()[3];
 
-                k = 0;
-                for (auto prev_nuc = 0; prev_nuc < 4; ++prev_nuc) {
-                    for (auto next_nuc = 0; next_nuc < 4; ++next_nuc, ++k) {
-                        std::cout << "prev" << (int) prev_nuc << "next" << (int) next_nuc << "k" << (int) k << std::endl;
-                        new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, prev_nuc, next_nuc)] += fb.DJ_nuc_probs()[k];
-                        changed[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, prev_nuc, next_nuc)] = true;
-                    }
-                }
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 1, 0)] += fb.VD_nuc_probs()[4];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 1, 1)] += fb.VD_nuc_probs()[5];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 1, 2)] += fb.VD_nuc_probs()[6];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 1, 3)] += fb.VD_nuc_probs()[7];
+
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 2, 0)] += fb.VD_nuc_probs()[8];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 2, 1)] += fb.VD_nuc_probs()[9];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 2, 2)] += fb.VD_nuc_probs()[10];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 2, 3)] += fb.VD_nuc_probs()[11];
+
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 3, 0)] += fb.VD_nuc_probs()[12];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 3, 1)] += fb.VD_nuc_probs()[13];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 3, 2)] += fb.VD_nuc_probs()[14];
+                new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, 3, 3)] += fb.VD_nuc_probs()[15];
+
+
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 0, 0)] += fb.DJ_nuc_probs()[0];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 0, 1)] += fb.DJ_nuc_probs()[1];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 0, 2)] += fb.DJ_nuc_probs()[2];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 0, 3)] += fb.DJ_nuc_probs()[3];
+
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 1, 0)] += fb.DJ_nuc_probs()[4];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 1, 1)] += fb.DJ_nuc_probs()[5];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 1, 2)] += fb.DJ_nuc_probs()[6];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 1, 3)] += fb.DJ_nuc_probs()[7];
+
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 2, 0)] += fb.DJ_nuc_probs()[8];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 2, 1)] += fb.DJ_nuc_probs()[9];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 2, 2)] += fb.DJ_nuc_probs()[10];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 2, 3)] += fb.DJ_nuc_probs()[11];
+
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 3, 0)] += fb.DJ_nuc_probs()[12];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 3, 1)] += fb.DJ_nuc_probs()[13];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 3, 2)] += fb.DJ_nuc_probs()[14];
+                new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, 3, 3)] += fb.DJ_nuc_probs()[15];
+
+//                int k = 0;
+//                for (auto prev_nuc = 0; prev_nuc < 4; ++prev_nuc) {
+//                    for (auto next_nuc = 0; next_nuc < 4; ++next_nuc, ++k) {
+//                        new_param_vec[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, prev_nuc, next_nuc)] += fb.VD_nuc_probs()[k];
+//                        changed[new_param_vec.event_index(VDJ_VAR_DIV_INS_NUC, prev_nuc, next_nuc)] = true;
+//                    }
+//                }
+//
+//                k = 0;
+//                for (auto prev_nuc = 0; prev_nuc < 4; ++prev_nuc) {
+//                    for (auto next_nuc = 0; next_nuc < 4; ++next_nuc, ++k) {
+//                        std::cout << "prev" << (int) prev_nuc << "next" << (int) next_nuc << "k" << (int) k << std::endl;
+//                        new_param_vec[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, prev_nuc, next_nuc)] += fb.DJ_nuc_probs()[k];
+//                        changed[new_param_vec.event_index(VDJ_DIV_JOI_INS_NUC, prev_nuc, next_nuc)] = true;
+//                    }
+//                }
             }
 
 //            std::cout << "passed 3" << std::endl;

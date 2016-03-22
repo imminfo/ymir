b04850d780f776826ac995724a2a3e8ffde3c464
Vadim Nazarov <vdn@mailbox.org>
2016-03-22 23:12:21 +0300

    add --coding and --noncoding for the future

--- generate.py
+++ generate.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     ap.add_argument("-m", "--model", help = "either an alias of the one from available models in Ymir or a path to a folder with a model's .json file. For a list of available models in this Ymir distribution run $python3 pyymir.py -m", type = str, default = "")
     ap.add_argument("-c", "--count", help = "number of clonotypes to generate", default = 10000, type = int)
     ap.add_argument("-o", "--output", help = "output file", type = str, default = "./ymir_artif/out.ymir_gen.txt")
+    ap.add_argument("--coding", help = "generate only coding sequences", action = "store_true")
+    ap.add_argument("--noncoding", help = "generate only noncoding", action = "store_true")
     args = ap.parse_args()
 
     model, model_check = parse_model(args)

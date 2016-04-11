d1dbf8c400933b7289c9cbccb1ed48d8797dd3eb
Vadim Nazarov <vdn@mailbox.org>
2016-04-11 11:47:13 +0300

    more code

--- Ymir/src/clonotype_aa.h
+++ Ymir/src/clonotype_aa.h
@@ -12,7 +12,50 @@
 
 namespace ymir {
 
-    class ClonotypeAA;
+    class ClonotypeAA : public VDJAlignmentAA, ClonotypeBase {
+    public:
+
+        ClonotypeAA(const sequence_t &sequence,
+                    Recombination recomb,
+                    const segments_storage_t &segments,
+                    const CodonAlignmentVector &alignments,
+                    const n_D_alignments_storage_t &n_D_alignments)
+                : ClonotypeBase(sequence,
+                                AMINOACID,
+                                recomb,
+                                segments[0] && segments[1] && ((segments[2] && recomb == VDJ_RECOMB) || recomb == VJ_RECOMB)),
+                  VDJAlignmentAA(segments,
+                                 alignments,
+                                 n_D_alignments)
+        {
+        }
+
+
+        ClonotypeAA(const sequence_t &sequence,
+                    Recombination recomb,
+                    const VDJAlignmentAA &alignment)
+                : ClonotypeBase(sequence,
+                                AMINOACID,
+                                recomb,
+                                alignment.nVar() && alignment.nJoi() && ((alignment.nDiv() && recomb == VDJ_RECOMB) || recomb == VJ_RECOMB)),
+                  VDJAlignmentAA(alignment)
+        {
+        }
+
+
+        virtual ~ClonotypeAA()
+        {
+        }
+
+
+    protected:
+
+        ClonotypeAA()
+        {
+        }
+
+    };
+    
 
     typedef unique_ptr<ClonotypeAA> ClonotypeAAPtr;
 

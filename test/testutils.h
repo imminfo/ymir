da8c81a7ada78b54f5fcaad0d1d67b51013fe3cd
Vadim Nazarov <vdn@mailbox.org>
2016-11-23 23:57:39 +0300

    add make_test_events_vdj3()

--- test/testutils.h
+++ test/testutils.h
@@ -641,6 +641,172 @@ namespace ymir {
         return ModelParameterVector(VDJ_RECOMB, v1, v2, v3, v4, vector<prob_t>(), 0.03, true, v5);
     }
 
+
+    ModelParameterVector make_test_events_vdj3() {
+        vector<prob_t> v1;  // param vec
+        vector<event_ind_t> v2;  // lens vec
+        vector<event_ind_t> v3;  // event classes
+        vector<seq_len_t> v4;  // event family col numbers
+
+        // V
+        v1.push_back(.5); v1.push_back(.15); v1.push_back(.35);
+        v2.push_back(3);
+
+        v3.push_back(0);
+        v4.push_back(0);
+
+        // J-D
+        // J-D (3 Js - 2 Ds)
+        v1.push_back(.12); v1.push_back(.13);
+        v1.push_back(.13); v1.push_back(.14);
+        v1.push_back(.17); v1.push_back(.79);
+
+        v2.push_back(6);
+
+        v3.push_back(1);
+        v4.push_back(3);
+
+        // V del
+        // TGTGC
+        v1.push_back(.5); v1.push_back(.4); v1.push_back(.04); v1.push_back(.03); v1.push_back(.02); v1.push_back(.01);
+        v2.push_back(6);
+
+        // TGCG
+        v1.push_back(.35); v1.push_back(.05); v1.push_back(.25); v1.push_back(.1); v1.push_back(.25);
+        v2.push_back(5);
+
+        // TGA
+        v1.push_back(.1); v1.push_back(.2); v1.push_back(.3); v1.push_back(.4);
+        v2.push_back(4);
+
+        v3.push_back(2);
+        v4.push_back(0);
+        v4.push_back(0);
+        v4.push_back(0);
+
+        // J del
+        // TTT
+        v1.push_back(.15); v1.push_back(.15); v1.push_back(.3); v1.push_back(.4);
+        v2.push_back(4);
+
+        // ATTC
+        v1.push_back(.125); v1.push_back(.175); v1.push_back(.3); v1.push_back(.19); v1.push_back(.21);
+        v2.push_back(5);
+
+        // AATT
+        v1.push_back(.11); v1.push_back(.1); v1.push_back(.02); v1.push_back(.02); v1.push_back(.05); v1.push_back(.7);
+        v2.push_back(5);
+
+        v3.push_back(5);
+        v4.push_back(0);
+        v4.push_back(0);
+        v4.push_back(0);
+
+        // D1 dels
+        // GGCA
+        // max 1 D3' del
+        // max 1 D5' del
+        v1.push_back(.18); // first row
+        v1.push_back(.28);
+        v1.push_back(.36); // second row
+        v1.push_back(.18);
+
+        v2.push_back(4);
+        v4.push_back(2);
+
+        // D2 dels
+        // GGTT
+        // max 1 D3' del
+        // max 1 D5' del
+        // 3 rows 2 columns
+        v1.push_back(.22); v1.push_back(.21);
+        v1.push_back(.24); v1.push_back(.33);
+
+        v2.push_back(4);
+        v4.push_back(2);
+
+        v3.push_back(7);
+
+        // VD ins len
+        v1.push_back(.05); v1.push_back(.1); v1.push_back(.15); v1.push_back(.2); v1.push_back(.25); v1.push_back(.24); v1.push_back(.01);
+        v2.push_back(7);
+
+        v3.push_back(11);
+        v4.push_back(0);
+
+        // DJ ins len
+        v1.push_back(.1); v1.push_back(.24); v1.push_back(.25); v1.push_back(.05); v1.push_back(.01); v1.push_back(.15); v1.push_back(.2);
+        v2.push_back(7);
+
+        v3.push_back(12);
+        v4.push_back(0);
+
+        // VD ins nuc
+        // prev A
+        v1.push_back(.04); v1.push_back(.09); v1.push_back(.03); v1.push_back(.84);
+        v2.push_back(4);
+
+        v3.push_back(13);
+        v4.push_back(0);
+
+        // prev C
+        v1.push_back(.4); v1.push_back(.2); v1.push_back(.2); v1.push_back(.2);
+        v2.push_back(4);
+
+        v3.push_back(14);
+        v4.push_back(0);
+
+        // prev G
+        v1.push_back(.25); v1.push_back(.15); v1.push_back(.1); v1.push_back(.5);
+        v2.push_back(4);
+
+        v3.push_back(15);
+        v4.push_back(0);
+
+        // prev T
+        v1.push_back(.15); v1.push_back(.15); v1.push_back(.2); v1.push_back(.5);
+        v2.push_back(4);
+
+        v3.push_back(16);
+        v4.push_back(0);
+
+        // DJ ins nuc
+        // prev A
+        v1.push_back(.24); v1.push_back(.1); v1.push_back(.16); v1.push_back(.5);
+        v2.push_back(4);
+
+        v3.push_back(17);
+        v4.push_back(0);
+
+        // prev C
+        v1.push_back(.16); v1.push_back(.1); v1.push_back(.24); v1.push_back(.5);
+        v2.push_back(4);
+
+        v3.push_back(18);
+        v4.push_back(0);
+
+        // prev G
+        v1.push_back(.04); v1.push_back(.09); v1.push_back(.03); v1.push_back(.84);
+        v2.push_back(4);
+
+        v3.push_back(19);
+        v4.push_back(0);
+
+        // prev T
+        v1.push_back(.4); v1.push_back(.11); v1.push_back(.29); v1.push_back(.2);
+        v2.push_back(4);
+
+        v3.push_back(20);
+        v4.push_back(0);
+
+        vector<seq_len_t> v5; // min D genes len == 3
+        v5.push_back(3);
+        v5.push_back(3);
+        v5.push_back(3);
+
+        return ModelParameterVector(VDJ_RECOMB, v1, v2, v3, v4, vector<prob_t>(), 0.03, true, v5);
+    }
+
 }
 
 #endif //YMIR_TESTUTILS_H_H

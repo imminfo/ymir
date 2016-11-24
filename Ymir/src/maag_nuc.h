6a03e9be2dc9e2a132b265ccdc0cecd686277985
Vadim Nazarov <vdn@mailbox.org>
2016-11-24 12:48:53 +0300

    add check for recomb

--- Ymir/src/maag_nuc.h
+++ Ymir/src/maag_nuc.h
@@ -130,28 +130,32 @@ namespace ymir {
          */
         ///@{
         prob_t fullProbability(event_ind_t v_index, event_ind_t j_index) const {
-            std::vector<prob_t> arr_prob1(std::max(this->nodeRows(2), this->nodeColumns(2)), 0);
-            std::vector<prob_t> arr_prob2(std::max(this->nodeRows(2), this->nodeColumns(2)), 0);
+            if (_recomb == VJ_RECOMB) {
+                std::vector<prob_t> arr_prob1(std::max(this->nodeRows(2), this->nodeColumns(2)), 0);
+                std::vector<prob_t> arr_prob2(std::max(this->nodeRows(2), this->nodeColumns(2)), 0);
 
-            arr_prob1[0] = this->at(0, 0, v_index, j_index);
-            for (dim_t i = 0; i < this->nodeColumns(1); ++i) {
-                arr_prob2[i] = arr_prob1[0] * this->at(1, v_index, 0, i);
-            }
-
-            arr_prob1[0] = 0;
-
-            for (dim_t row_i = 0; row_i < this->nodeRows(2); ++row_i) {
-                for (dim_t col_i = 0; col_i < this->nodeColumns(2); ++col_i) {
-                    arr_prob1[col_i] += this->at(2, 0, row_i, col_i) * arr_prob2[row_i];
+                arr_prob1[0] = this->at(0, 0, v_index, j_index);
+                for (dim_t i = 0; i < this->nodeColumns(1); ++i) {
+                    arr_prob2[i] = arr_prob1[0] * this->at(1, v_index, 0, i);
                 }
-            }
 
-            arr_prob2[0] = 0;
-            for (dim_t i = 0; i < this->nodeRows(3); ++i) {
-                arr_prob2[0] += arr_prob1[i] * this->at(3, j_index, i, 0);
-            }
+                arr_prob1[0] = 0;
 
-            return arr_prob2[0];
+                for (dim_t row_i = 0; row_i < this->nodeRows(2); ++row_i) {
+                    for (dim_t col_i = 0; col_i < this->nodeColumns(2); ++col_i) {
+                        arr_prob1[col_i] += this->at(2, 0, row_i, col_i) * arr_prob2[row_i];
+                    }
+                }
+
+                arr_prob2[0] = 0;
+                for (dim_t i = 0; i < this->nodeRows(3); ++i) {
+                    arr_prob2[0] += arr_prob1[i] * this->at(3, j_index, i, 0);
+                }
+
+                return arr_prob2[0];
+            } else {
+                return 0;
+            }
         }
 
         prob_t fullProbability(event_ind_t v_index, event_ind_t d_index, event_ind_t j_index) const {

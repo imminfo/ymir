2aa671c419ea3c87135173cc2084bb66e9b72820
Vadim Nazarov <vdn@mailbox.org>
2016-12-10 13:34:26 +0300

    change scores a bit

--- scripts/compute.cpp
+++ scripts/compute.cpp
@@ -58,13 +58,13 @@ int main(int argc, char* argv[]) {
                                                                VDJAlignmentEventScore(AlignmentEventScore(1, -1, 1),
                                                                                       AlignmentEventScore(1, -1, 1),
                                                                                       AlignmentEventScore(1, -1, 1)),
-                                                               VDJAlignmentScoreThreshold(2, 2, 2));
+                                                               VDJAlignmentScoreThreshold(6, 3, 5));
 
-        auto vdj_aligner_parameters_aa =  VDJAlignerParameters(3,
-                                                               VDJAlignmentEventScore(AlignmentEventScore(1, -1, 1),
-                                                                                      AlignmentEventScore(1, -1, 1),
-                                                                                      AlignmentEventScore(1, -1, 1)),
-                                                               VDJAlignmentScoreThreshold(1, 1, 1));
+//        auto vdj_aligner_parameters_aa =  VDJAlignerParameters(3,
+//                                                               VDJAlignmentEventScore(AlignmentEventScore(1, -1, 1),
+//                                                                                      AlignmentEventScore(1, -1, 1),
+//                                                                                      AlignmentEventScore(1, -1, 1)),
+//                                                               VDJAlignmentScoreThreshold(1, 1, 1));
 
         if (parser.openAndParse(in_file_path,
                                 &cloneset,
